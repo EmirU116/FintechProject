add8d877b4dbf933bf32b80829030afd77cfa7ab
Emir <emir_ulusoy_49@hotmail.com>
2025-12-05 15:19:06 +0100

    deleting a readme file

--- test/FintechProject.Tests/obj/Debug/net8.0/FintechProject.Tests.AssemblyInfo.cs
+++ test/FintechProject.Tests/obj/Debug/net8.0/FintechProject.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FintechProject.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91aabeb0e52aa266fa2add07abf49da0c901d656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d937fad9640754a33182b3cde3616afe52fe3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FintechProject.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FintechProject.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
